892e92b081b6b00fc3792658f51dc2296927bead
Ewald van der Westhuizen <ewaldvdw@gmail.com>
2019-02-06 16:24:20 +0200

    Adding two bash scripts that are used for the evaluation pipline running on a remote machine in a docker image. Still work in progress...

---
+++ evaluation/run_abx_eval_remotely.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script will run the ABX evaluation on a remote machine which contains the zs2019 baseline docker image.
+# The only argument to this script is the filename of the hknpz (.npz) feature file.
+innpzfn=$1
+innpzfn_bn=$(basename $1)
+
+# Copy the .npz file into a tmp dir on the remote machine.
+tmpdir=$(basename $(mktemp -u))
+echo ${tmpdir}
+echo "Copying ${innpzfn}..."
+rsync -P ${innpzfn} suzero@146.232.221.153:~/${tmpdir}/
+
+# kick off the evaluation process with an ssh command
+#ssh suzero@146.232.221.153 bash ~/run_abx_eval.sh ~/${tmpdir}/${innpzfn}
+ssh suzero@146.232.221.153 bash '~/run_abx_eval.sh' '~/'"${tmpdir}/${innpzfn}"
+
+# Capture the out and print
+
+
