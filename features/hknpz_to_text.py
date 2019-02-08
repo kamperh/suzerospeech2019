73f7cec42523857a8746c09f445bdce5ddc8dbfb
Ewald van der Westhuizen <ewaldvdw@gmail.com>
2019-02-08 23:00:33 +0200

    Added some checks to the npz to text format conversion script. It reports on vector dimensionality, number of unique symbols (or vector types), continuous or discreet vectors, and the vector data type.

--- features/hknpz_to_text.py
+++ features/hknpz_to_text.py
@@ -37,9 +37,77 @@ def check_argv():
         sys.exit(1)
     return parser.parse_args()
 
+def print_report(feat_dict):
+    print "Number of unique symbols (feature vector types):", fd_number_of_unique_symbols(feat_dict)
+    print "Is continuous:", fd_iscontinuous(feat_dict)
+    print "Feature dimensionality:", fd_dimensionality(feat_dict)
+    print "Feature data type:", fd_dtype(feat_dict)
 
-def join_segments():
-    pass
+
+#-----------------------------------------------------------------------#
+#                   FEATURE DICTIONARY FUNCTIONS                        #
+#-----------------------------------------------------------------------#
+
+def fd_iscontinuous(feat_dict):
+    """
+    Estimate if the features in the dictionary are continuous or discreet.
+    """
+    elementvalueset = set([])
+    for akey, autt in feat_dict.iteritems():
+        elementvalueset.update(autt.flatten().tolist())
+        if len(elementvalueset) > 2:
+            return True
+    return False
+
+
+def fd_number_of_unique_symbols(feat_dict):
+    """
+    Returns the number of unique feature types in the dictionary.
+    """
+    symbolset = set([])
+    for akey, autt in feat_dict.iteritems():
+        for avec in autt:
+            symbolset.add(tuple(avec.tolist()))
+    return len(symbolset)
+
+def fd_dimensionality(feat_dict):
+    """
+    Returns a list of vector dimensionalities found in the dictionary.
+    We expect only one value to be returned in the list, or else there
+    may be an error in the set of vectors.
+    """
+    dimset = set([])
+    for akey, autt in feat_dict.iteritems():
+        for avec in autt:
+            dimset.add(avec.shape)
+    return list(dimset)
+
+
+def fd_dtype(feat_dict):
+    """
+    Returns a list of data types found in the dictionary.
+    We expect only one value to be returned in the list, or else there
+    may be an error in the set of vectors.
+    """
+    dtypeset = set([])
+    for akey, autt in feat_dict.iteritems():
+        for avec in autt:
+            dtypeset.add(str(avec.dtype))
+    return list(dtypeset)
+
+
+#-----------------------------------------------------------------------#
+#                         FEATURE FUNCTIONS                             #
+#-----------------------------------------------------------------------#
+
+def fs_iscontinuous(autt):
+    """
+    Estimate if the features of an utterances are continuous or discreet.
+    """
+    if len(set(autt.flatten().tolist())) > 2:
+        return True
+    else:
+        return False
 
 #-----------------------------------------------------------------------------#
 #                                MAIN FUNCTION                                #
@@ -50,6 +118,8 @@ def main():
 
     print("Reading:", args.npz_fn)
     feat_dict = np.load(args.npz_fn)
+
+    print_report(feat_dict)
 
     if not path.isdir(args.output_dir):
         os.makedirs(args.output_dir)
